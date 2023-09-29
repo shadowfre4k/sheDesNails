b35cb1fdea91939994fa6e56a103cf5e5961b1fe
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-09-28 20:03:34 -0700

    skeleton server side home page and router up and running

---
+++ shedesnails/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import Navbar from "./components/Navbar";
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ shedesnails/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+      <div>
+        <h1>World</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
