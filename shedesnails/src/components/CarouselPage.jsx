8a0103b292b93029b9ede8ab05b03347f1b4e8e9
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-10-07 13:14:23 -0700

    buttons

--- shedesnails/src/components/CarouselPage.jsx
+++ shedesnails/src/components/CarouselPage.jsx
@@ -4,17 +4,23 @@ import Test from "../assets/react.svg";
 import "./CarouselPage.css";
 function CarouselPage() {
   return (
-    <Splide className="carousel">
-      <SplideSlide>
-        <img src={Test} alt="Image 1" />
-      </SplideSlide>
-      <SplideSlide>
-        <img src={Test} alt="Image 1" />
-      </SplideSlide>
-      <SplideSlide>
-        <img src={Test} alt="Image 1" />
-      </SplideSlide>
-    </Splide>
+    <div>
+      <Splide className="carousel">
+        <SplideSlide>
+          <img src={Test} alt="Image 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src={Test} alt="Image 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src={Test} alt="Image 1" />
+        </SplideSlide>
+      </Splide>
+      <div className="buttons">
+        <button>button 1</button>
+        <button>button 2</button>
+      </div>
+    </div>
   );
 }
 
