f71889c92842e7aff63c2e246a4b272b56c7525d
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-10-09 19:11:10 -0700

    created carousel added pictures

--- shedesnails/src/components/CarouselPage.jsx
+++ shedesnails/src/components/CarouselPage.jsx
@@ -1,21 +1,33 @@
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
-import Test from "../assets/react.svg";
+import sheDes1 from "../assets/sheDesPics/sheDes1.jpg";
+import sheDes2 from "../assets/sheDesPics/sheDes2.jpg";
+import sheDes3 from "../assets/sheDesPics/sheDes3.jpg";
+import sheDes4 from "../assets/sheDesPics/sheDes4.jpg";
+import sheDes5 from "../assets/sheDesPics/sheDes5.jpg";
 import "./CarouselPage.css";
 function CarouselPage() {
   return (
     <div>
-      <Splide className="carousel">
-        <SplideSlide>
-          <img src={Test} alt="Image 1" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={Test} alt="Image 1" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={Test} alt="Image 1" />
-        </SplideSlide>
-      </Splide>
+      <div className="carouselContainer">
+        <Splide className="carousel">
+          <SplideSlide>
+            <img src={sheDes1} alt="Image 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={sheDes2} alt="Image 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={sheDes3} alt="Image 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={sheDes4} alt="Image 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={sheDes5} alt="Image 1" />
+          </SplideSlide>
+        </Splide>
+      </div>
       <div className="buttons">
         <button>button 1</button>
         <button>button 2</button>
