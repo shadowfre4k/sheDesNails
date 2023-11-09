ba880db149a5b9d22f300c446caf293bd56bd27e
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-11-08 20:07:06 -0800

    header cleanup

--- shedesnails/src/components/Navbar.jsx
+++ shedesnails/src/components/Navbar.jsx
@@ -3,14 +3,16 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <>
-      <div className="shrinkContainer">
-        <div className="navContainer">
-          <ul className="linkContainer">
-            <p className="linkItem">About</p>
-            <p className="linkItem">Policies</p>
-            <p className="linkItem">Book</p>
-          </ul>
-          <div className="brandContainer">She Des Nails</div>
+      <div className="bigContainer">
+        <div className="shrinkContainer">
+          <div className="navContainer">
+            <div className="linkContainer">
+              <p className="linkItem">About</p>
+              <p className="linkItem">Policies</p>
+              <p className="linkItem">Book</p>
+            </div>
+            <div className="brandContainer">She Des Nails</div>
+          </div>
         </div>
       </div>
     </>
