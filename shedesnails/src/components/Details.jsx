0c0a769e31469f4f2cbe2cf21476334fb4909ecd
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-11-21 20:54:39 -0800

    added map place holder and more styling

--- shedesnails/src/components/Details.jsx
+++ shedesnails/src/components/Details.jsx
@@ -3,16 +3,18 @@ import "./Details.css";
 const Details = () => {
   return (
     <>
-      <div></div>
-      <div>
-        <p className="location">Location</p>
-        <p className="contactDetails">
-          {`7565 Mission Gorge Road (Inside Sugar Plum Skincare)
-            San Diego, CA 92120
-            (858)342-8825
-            shedesnails@gmail.com`}
-        </p>
-        <p className="directions">Get directions</p>
+      <div className="detailsContainer">
+        <div className="details">
+          <p className="location">Location</p>
+          <p className="address">
+            {`7565 Mission Gorge Road (Inside Sugar Plum Skincare)
+            San Diego, CA 92120`}
+          </p>
+          <p className="phoneNumber"> (858)342-8825 </p>
+          <p className="email"> shedesnails@gmail.com </p>
+          <p className="directions"> Get directions </p>
+        </div>
+        <div className="details">map place holder</div>
       </div>
     </>
   );
